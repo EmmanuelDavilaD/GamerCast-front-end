55616db30f6304da5c61a5fe9cc0f90c55181a51
Emmanueldavila-developer <emmanuelduque04@Gmail.com>
2017-07-30 12:03:07 -0300

    GamerCast
    Web-application-frontend
    
    "babel-plugin-syntax-async-functions":
        "babel-plugin-transform-regenerator"
        "babel-preset-es2015"
        "babelify"
        "browserify"
        "empty-element"
        "express"
        "gulp"
        "gulp-rename"
        "gulp-sass"
        "vinyl-source-stream"
        "watchify"
    "axios"
        "babel-polyfill"
        "empty-element"
        "express"
        "file-extension"
        "intl"
        "intl-messageformat"
        "intl-relativeformat"
        "materialize-css"
        "multer"
        "page"
        "pug"
        "superagent"
        "title"
        "yo-yo"

---
+++ server.js
@@ -0,0 +1,76 @@
+var express = require('express');
+var multer  = require('multer');
+var ext = require('file-extension');
+
+var storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, './uploads')
+  },
+  filename: function (req, file, cb) {
+    cb(null, +Date.now() + '.' + ext(file.originalname))
+  }
+})
+
+var upload = multer({ storage: storage }).single('picture');
+
+var app = express();
+
+app.set('view engine', 'pug');
+
+app.use(express.static('public'));
+
+app.get('/', function (req, res) {
+  res.render('index', { title: 'Platzigram' });
+})
+
+app.get('/signup', function (req, res) {
+  res.render('index', { title: 'Platzigram - Signup' });
+})
+
+app.get('/signin', function (req, res) {
+  res.render('index', { title: 'Platzigram - Signin' });
+})
+
+app.get('/api/pictures', function (req, res, next) {
+  var pictures = [
+    {
+      user: {
+        username: 'zelda',
+        avatar: 'https://bnetproduct-a.akamaihd.net//dg/5234/96DC596B381E8873ECA60094384AD1B6B01B98B2.jpg'
+      },
+      url: 'https://assets.vg247.com/current//2016/08/world_of_warcraft_legion_release_time_eu_australia_countdown_launch.jpg',
+      likes: 0,
+      liked: false,
+      createdAt: new Date().getTime()
+    },
+    {
+      user: {
+        username: 'Chadwick',
+        avatar: 'https://bnetproduct-a.akamaihd.net//dg/5234/96DC596B381E8873ECA60094384AD1B6B01B98B2.jpg'
+      },
+      url: 'https://assets.vg247.com/current//2016/08/world_of_warcraft_legion_release_time_eu_australia_countdown_launch.jpg',
+      likes: 1,
+      liked: true,
+      createdAt: new Date().setDate(new Date().getDate() - 10)
+    }
+  ];
+
+  setTimeout(function () {
+    res.send(pictures);
+  }, 2000)
+});
+
+app.post('/api/pictures', function (req, res) {
+  upload(req, res, function (err) {
+    if (err) {
+      return res.send(500, "Error uploading file");
+    }
+    res.send('File uploaded');
+  })
+})
+
+app.listen(3000, function (err) {
+  if (err) return console.log('Hubo un error'), process.exit(1);
+
+  console.log('Gamercast escuchando en el puerto 3000');
+})
---
+++ src/translate/es.js
@@ -0,0 +1,20 @@
+module.exports ={
+  'likes': '{likes, number} me gusta',
+  'logout': 'Salir',
+  'english': 'Inglés',
+  'spanish': 'Español',
+  'signup.subheading': 'Regístrate para ver fotos de tus amigos estudiando en Gamercast',
+  'signup.facebook': 'Iniciar sesión con Facebook',
+  'signup.text': 'Iniciar Sesión',
+  'email': 'Correo electrónico',
+  'username': 'Nombre de Usuario',
+  'fullname': 'Nombre completo',
+  'password': 'contraseña',
+  'signup.call-to-action': 'Regístrate',
+  'signup.have-account': '¿Tienes una cuenta?',
+  'signin':'Entrar',
+  'signin.not-have-account': '¿No tienes una cuenta?',
+  'language':'Idioma',
+  'upload-picture': 'Subir Foto',
+  'upload': 'subir'
+}
